459a1cdb04109aa7d7c9112bf9474135752bcefa
sg <srfngrc@outlook.com>
2019-05-15 17:34:49 -0400

    holaholahola

---
+++ TestApp01/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp01
+{
+    public partial class Form11 : Form
+    {
+        public Form11()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // somerrorcode;
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            checkBox1.Text = "other value";
+        }
+    }
+}
